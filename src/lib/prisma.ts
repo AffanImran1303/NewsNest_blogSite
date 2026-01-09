2138ae87a013309821325af6b9059cb2379f1011
Affan Imran <115869839+AffanImran1303@users.noreply.github.com>
2026-01-09 23:48:20 +0500

    Implement singleton pattern for Prisma client
    Refactor prisma client initialization to use a singleton pattern.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,3 @@
-
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 import { PrismaClient } from '@prisma/client';
@@ -7,13 +6,18 @@ const connectionString = `${process.env.DATABASE_URL}`;
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+// Use a self-referencing interface for the global object to satisfy TS
+const prismaClientSingleton = () => {
+  return new PrismaClient({ adapter });
+};
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter,
-  });
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
